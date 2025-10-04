1f089ba9342e3f1a0d9318c7d80ad44dfc4cccee
Okah Prosper <prosperoluwadamilare@gmail.com>
2025-10-04 21:45:11 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: '#6C5CE7',
+        bg1: '#071021'
+      },
+      fontFamily: {
+        inter: ['Inter', 'ui-sans-serif', 'system-ui']
+      }
+    }
+  },
+  plugins: []
+}
