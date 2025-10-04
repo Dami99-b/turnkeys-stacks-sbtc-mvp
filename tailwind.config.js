1ca35d06346229e0891abfca0fe5e8803fe8912d
Okah Prosper <prosperoluwadamilare@gmail.com>
2025-10-04 23:21:55 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}"
+  ],
   theme: {
-    extend: {
-      colors: { neon: "#00FFC6", brand: "#6C5CE7", darkbg: "#05061a" },
-      fontFamily: { inter: ["Inter", "ui-sans-serif", "system-ui"] }
-    }
+    extend: {}
   },
   plugins: []
-};
+}
