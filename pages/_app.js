e7cffb97377c43eb061be342fc0fdd82ed2726ce
Okah Prosper <prosperoluwadamilare@gmail.com>
2025-10-04 18:25:36 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,5 @@
 import '../styles/globals.css';
-import ToastRoot from '../components/ToastRoot';
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <ToastRoot />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
